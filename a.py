8869d381096b2643bb2499965c9b0a92fa35eed8
laxmi20004 <117802759+laxmi20004@users.noreply.github.com>
2022-11-16 12:56:17 +0530

    Create a.py

---
+++ a.py
@@ -0,0 +1,7 @@
+def decimalToBinary(n):
+    return bin(n).replace("0b", "")
+  
+if __name__ == '__main__':
+    print(decimalToBinary(8))
+    print(decimalToBinary(18))
+    print(decimalToBinary(7))
